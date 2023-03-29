48af202254e38a56e1a22e9a08f799db5e8d67ba
BurgersMcFly <burgersmcfly@outlook.com>
2023-03-29 23:09:34 +0300

    Add files via upload

---
+++ NoSleepingLoadingScreen.cs
@@ -0,0 +1,52 @@
+﻿// NoSleepingLoadingScreen, Valheim mod that disables the loading screen when sleeping
+// Copyright (C) 2022 BurgersMcFly
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace NoSleepingLoadingScreen
+{
+    [BepInPlugin(pluginGUID, pluginName, pluginVersion)]
+    public class NoSleepingLoadingScreen : BaseUnityPlugin
+    {
+        const string pluginGUID = "NoSleepingLoadingScreen";
+        const string pluginName = "No Sleeping Loading Screen";
+        const string pluginVersion = "1.0.0";
+        public static readonly Harmony harmony = new Harmony("NoSleepingLoadingScreen");
+        void Awake()
+        {
+            harmony.PatchAll();
+
+        }
+        void OnDestroy()
+        {
+            harmony.UnpatchSelf();
+        }
+
+
+        [HarmonyPatch(typeof(Hud), "UpdateBlackScreen")]
+        public static class RemoveLoadingScreen
+        {
+            private static void Prefix(ref CanvasGroup ___m_loadingScreen, ref Player player,ref GameObject ___m_sleepingProgress)
+            {
+                if (player != null && player.IsSleeping())
+                {
+                    ___m_sleepingProgress.SetActive(value: false);
+                    ___m_loadingScreen.alpha = 0f;
+                }
+            }
+
+        }
+    }
+}
